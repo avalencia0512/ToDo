87a0977cfd14f3526fb37989a8d9f11fdcb84164
Britney O'Dell <brodell@microsoft.com>
2021-10-02 13:49:57 -0700

    Adding page one with naviagtion

--- ToDoApp/routes/index.js
+++ ToDoApp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET page one. */
+router.get('/pageOne', function(req, res, next) {
+  res.render('pageOne.jade', { title: 'Page 1' });
+});
+
 module.exports = router;
